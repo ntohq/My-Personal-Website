1db6030d43a10519b5a031a2748b48624212b1c8
Wesley Ford <mangofordw@gmail.com>
2020-07-16 14:03:27 -0400

    v 0.0.0.7

---
+++ public_html/src/components/greeting.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="section has-text-left">
+      <h2 class="title is-1">Welcome</h2>
+      <div class="media box" style="z-index: 1;">
+        <figure class="image is-48x48 media-left">
+          <img class="is-rounded" src="@/assets/animatedMe.png"/>
+        </figure>
+        <p class="media-content">
+          Oh, hey there! You've found my website! Here I showcase my projects, posts, articles, and career. I hope all the resources that I have here are helpful to you. All your feedback is appreciated.
+          Thank you, and God bless. <br>     ~Wesley Ford 
+        </p>
+      </div>
+    </section>
+</template>
---
+++ public_html/src/pages/404.vue
@@ -0,0 +1,17 @@
+<template>
+  <mainLayout>
+    <section class="section is-large">
+      <h2 class="has-text-centered title is-1">Oh no! It's the jedi</h2>
+      <h2 class="has-text-centered title">The resource you are looking for is not here. You will go back now.</h2>
+      <p><i>This resouce is not here. I will go back now</i></p>
+    </section>
+  </mainLayout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: '404 Error'
+  },
+}
+</script>
