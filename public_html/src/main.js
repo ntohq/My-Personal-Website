0710aa426c194fd5fdda99b4326a0fc82fed9c5f
Wesley Ford <mangofordw@gmail.com>
2020-07-18 21:53:39 -0400

    Desgin update

--- public_html/src/main.js
+++ public_html/src/main.js
@@ -8,6 +8,7 @@ import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
 export default function (Vue, { router, head, isClient }) {
+
   // Set default layout as a global component
   Vue.component('mainLayout', DefaultLayout)
   Vue.component('Layout', HomeLayout)
--- public_html/src/pages/About.vue
+++ public_html/src/pages/About.vue
@@ -1,8 +1,10 @@
 <template>
-  <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
-  </Layout>
+  <mainLayout>
+    <div class="hero">
+      <h2 class="title">About Me</h2>
+      <p></p>
+    </div>
+  </mainLayout>
 </template>
 
 <script>
