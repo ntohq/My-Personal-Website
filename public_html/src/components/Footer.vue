163981da915bd91da25215eaa87db177469e68e6
Wesley Ford <mangofordw@gmail.com>
2021-02-15 21:13:42 -0600

    Update Footer.vue

--- public_html/src/components/Footer.vue
+++ public_html/src/components/Footer.vue
@@ -1,15 +1,27 @@
 <template>
-  <footer class="footer has-background-black" >
-    <div style="margin: auto; max-width: 125em; padding-left: 15px; padding-right: 15px;">
-    <div class="has-text-centered">
-        <h2 class="title is-2 has-text-white" style="margin-right: 15px;">Contact</h2>
-        <h2 class="title is-4 has-text-white">Wesley Ford</h2>
-        <h2 class="title is-6 has-text-white">Email :</h2>
-        <ul class="level-item" style="display: inline-block; margin-right: 20px;">
+  <footer class="footer has-background-black level">
+    <div class="level-item has-text-centered" style="display: flex; flex-direction: column;">
+    <h2 class="title is-4 has-text-white">Wesley Ford</h2>
+    <h2 class="title is-6 has-text-white"></h2>
+    </div>
+    <aside class="level-item has-text-centered" style="display: flex; flex-direction: column;">
+      <h2 class="title is-2 has-text-white" style="margin-right: 15px">Links</h2>
+      <div style="display: flex;">
+        <div style="flex: 1;">
+          <ol class="level-item" style="display: inline-block; margin-right: 20px">
             <li><a href="https://github.com/ntohq">github</a></li>
             <li><a href="https://dev.to/ntohq">dev.to</a></li>
-        </ul>
+            <li><a href="https://www.linkedin.com/in/wesley-ford-webdev">LinkedIn</a></li>
+          </ol>
+        </div>
+        <div style="flex: 1;">
+          <ol class="level-item" style="display: inline-block; margin-right: 20px">
+            <li><a href="https://github.com/ntohq">github</a></li>
+            <li><a href="https://dev.to/ntohq">dev.to</a></li>
+            <li><a href="https://www.linkedin.com/in/wesley-ford-webdev">LinkedIn</a></li>
+          </ol>
+        </div>
       </div>
-    </div>
+    </aside>
   </footer>
 </template>
